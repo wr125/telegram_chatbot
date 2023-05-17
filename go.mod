aa1db0578e890d9bbddc62f827f7671a77744005
Robert Willis <robert.willis010@gmail.com>
2023-05-17 12:48:52 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/wr125/telegram_chatbot
+
+go 1.20
+
+require (
+	github.com/PaulSonOfLars/gotgbot/v2 v2.0.0-rc.20
+	github.com/joho/godotenv v1.5.1
+)
---
+++ routes/routes.go
@@ -0,0 +1,40 @@
+package routes
+
+import (
+	"net/http"
+	"text/template"
+
+	"github.com/PaulSonOfLars/gotgbot/v2/ext"
+)
+
+var indexTmpl = template.Must(template.ParseFiles("index.html"))
+
+func Index(webappURL string) func(writer http.ResponseWriter, request *http.Request) {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		err := indexTmpl.ExecuteTemplate(writer, "index.html", struct {
+			WebAppURL string
+		}{
+			WebAppURL: webappURL,
+		})
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			writer.Write([]byte(err.Error()))
+		}
+	}
+}
+
+func Validate(token string) func(writer http.ResponseWriter, request *http.Request) {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		ok, err := ext.ValidateWebAppQuery(request.URL.Query(), token)
+		if err != nil {
+			writer.WriteHeader(http.StatusBadRequest)
+			writer.Write([]byte("validation failed; error: " + err.Error()))
+			return
+		}
+		if ok {
+			writer.Write([]byte("validation success; user is authenticated."))
+		} else {
+			writer.Write([]byte("validation failed; data cannot be trusted."))
+		}
+	}
+}
